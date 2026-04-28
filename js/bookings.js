639bbfca5812ab2e40d8afbf2a5e1c11efbbf4f0
angeliceralde-prog <angeliceralde@gmail.com>
2026-04-28 22:46:44 +0800

    Update bookings.js

--- js/bookings.js
+++ js/bookings.js
@@ -1,1 +1,14 @@
+function createNewBooking() {
+    dashboardData.income += 540;
+    dashboardData.bookingsCount += 1;
+    dashboardData.activeGuests += 1;
 
+    dashboardData.recentBookings.unshift({
+        guest: "New Guest",
+        dates: "Aug 10 - 12",
+        id: "#INV-" + Date.now(),
+        amount: 540
+    });
+
+    refreshUI();
+}
